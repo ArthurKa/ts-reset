a4ab41ac25cd7637868fe7ad6ffc373ba12fc968
ArthurKa <arthur.katruk@gmail.com>
2023-08-18 23:16:06 +0300

    feat: fix `JSON.stringify(undefined)` `string` type

--- src/JSON.ts
+++ src/JSON.ts
@@ -6,6 +6,12 @@ declare module 'json5' {
     text: string,
     reviver?: (this: any, key: string, value: any) => any,
   ): T;
+
+  function stringify<T>(value: T, replacer?: (this: any, key: string, value: any) => any, space?: string | number): (
+    T extends undefined
+      ? undefined
+      : string
+  );
 }
 
 declare global {
@@ -14,5 +20,11 @@ declare global {
       text: string,
       reviver?: (this: any, key: string, value: any) => any,
     ): T;
+
+    stringify<T>(value: T, replacer?: (this: any, key: string, value: any) => any, space?: string | number): (
+      T extends undefined
+        ? undefined
+        : string
+    );
   }
 }
--- tests/JSON.test-d.ts
+++ tests/JSON.test-d.ts
@@ -25,3 +25,19 @@ expectType<123>(res5);
 const res6 = JSON5.parse<123>('');
 //    ^?
 expectType<123>(res6);
+
+const res7 = JSON.stringify(undefined);
+//    ^?
+expectType<undefined>(res7);
+
+const res8 = JSON5.stringify(undefined);
+//    ^?
+expectType<undefined>(res8);
+
+const res9 = JSON.stringify(undefined as undefined | number);
+//    ^?
+expectType<string | undefined>(res9);
+
+const res10 = JSON5.stringify(undefined as undefined | number);
+//    ^?
+expectType<string | undefined>(res10);
