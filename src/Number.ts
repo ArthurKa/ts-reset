2f3c4b451838613ab4a5b65b261f07d376481659
ArthurKa <arthur.katruk@gmail.com>
2023-09-21 18:47:39 +0300

    feat: extend NumberConstructor with all string insertable types

--- src/Number.ts
+++ src/Number.ts
@@ -1,7 +1,16 @@
-export {};
+import { StringInsertable } from './utils';
 
 declare global {
   interface NumberConstructor {
     (): 0;
+    <T extends StringInsertable>(e: T): (
+      T extends null | false
+        ? 0
+        : T extends true
+          ? 1
+          : `${T}` extends `${infer R extends number}`
+            ? R
+            : number
+    );
   }
 }
--- tests/Number.test-d.ts
+++ tests/Number.test-d.ts
@@ -1,5 +1,40 @@
 import { expectType } from 'tsd';
+import { tuple } from '@arthurka/ts-utils';
 
 const res1 = Number();
 //    ^?
 expectType<0>(res1);
+
+const res2 = Number(123);
+//    ^?
+expectType<123>(res2);
+
+const res3 = Number(false);
+//    ^?
+expectType<0>(res3);
+
+const res4 = Number(false);
+//    ^?
+expectType<0>(res4);
+
+const res5 = Number(false as boolean);
+//    ^?
+expectType<0 | 1>(res5);
+
+const numbers = tuple(
+  //  ^?
+  Number(undefined),
+  Number(Infinity),
+  Number(-Infinity),
+  Number(NaN),
+  Number('123' as string),
+);
+expectType<[number, number, number, number, number]>(numbers);
+
+const res6 = Number(200n);
+//    ^?
+expectType<200>(res6);
+
+const res7 = Number('1234');
+//    ^?
+expectType<1234>(res7);
