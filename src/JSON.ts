e38093132d8fd6adb321b23536b0214a6500a1c4
ArthurKa <arthur.katruk@gmail.com>
2023-11-27 14:53:08 +0200

    refactor: make some code refactoring

--- src/JSON.ts
+++ src/JSON.ts
@@ -2,30 +2,30 @@ import 'json5';
 
 declare module 'json5' {
   // WARN: `@ts-ignore` directive will be added here on compile
-  function parse<T>(
-    text: string,
-    reviver?: (this: any, key: string, value: any) => any,
-  ): T;
-
-  // WARN: `@ts-ignore` directive will be added here on compile
   function stringify<T>(value: T, replacer?: (this: any, key: string, value: any) => any, space?: string | number): (
     T extends undefined
       ? undefined
       : string
   );
+
+  // WARN: `@ts-ignore` directive will be added here on compile
+  function parse<T>(
+    text: string,
+    reviver?: (this: any, key: string, value: any) => any,
+  ): T;
 }
 
 declare global {
   interface JSON {
-    parse<T>(
-      text: string,
-      reviver?: (this: any, key: string, value: any) => any,
-    ): T;
-
     stringify<T>(value: T, replacer?: (this: any, key: string, value: any) => any, space?: string | number): (
       T extends undefined
         ? undefined
         : string
     );
+
+    parse<T>(
+      text: string,
+      reviver?: (this: any, key: string, value: any) => any,
+    ): T;
   }
 }
