81bb419789a0e0a1ad53d1668dda91e56859d710
ArthurKa <arthur.katruk@gmail.com>
2024-05-19 15:58:07 +0300

    feat: improve Promise.then typings

--- src/Promise.ts
+++ src/Promise.ts
@@ -1,6 +1,9 @@
 declare global {
   interface Promise<T> {
     catch<TResult = never>(onrejected?: ((reason: unknown) => TResult | PromiseLike<TResult>) | undefined | null): Promise<T | TResult>;
+
+    then(onfulfilled?: null | undefined): Promise<T>;
+    then<TResult = T>(onfulfilled: (value: T) => TResult | PromiseLike<TResult>): Promise<TResult>;
     then<TResult1 = T, TResult2 = never>(
       onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null,
       onrejected?: ((reason: unknown) => TResult2 | PromiseLike<TResult2>) | undefined | null
--- tests/Promise.test-d.ts
+++ tests/Promise.test-d.ts
@@ -1,15 +1,37 @@
 import { expectType } from 'tsd';
 
-new Promise(() => {}).catch(e => {
-  //                        ^?
+Promise.resolve(123).catch(e => {
   expectType<unknown>(e);
 });
 
-new Promise(() => {}).then(
+Promise.resolve(123 as const).then(
   e => {
-    expectType<unknown>(e);
+    expectType<123>(e);
   },
   e => {
     expectType<unknown>(e);
   },
 );
+
+declare const param1: boolean | null;
+declare const param2: boolean | undefined;
+
+const asd1 = param1 ?? await Promise.resolve(123 as const).then();
+expectType<boolean | 123>(asd1);
+//                        ^?
+
+const asd2 = param1 ?? await Promise.resolve(123 as const).then(undefined);
+expectType<boolean | 123>(asd2);
+//                           ^?
+
+const asd3 = param2 ?? await Promise.resolve(123 as const).then(null);
+expectType<boolean | 123>(asd3);
+//                           ^?
+
+const asd4 = param1 ?? await Promise.resolve(123 as const).then(null);
+expectType<boolean | 123>(asd4);
+//                           ^?
+
+const asd5 = param1 ?? await Promise.resolve(123 as const).then(e => String(e));
+expectType<boolean | '123'>(asd5);
+//                           ^?
